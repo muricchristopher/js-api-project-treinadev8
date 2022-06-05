2aa468cec2d4e2d61299bf1ad28b42bf210f4a9e
murichristopher <murichristopher@outlook.com>
2022-06-05 19:18:57 -0300

    Possibilita o filtro de usuários por país

--- app.js
+++ app.js
@@ -1,21 +1,56 @@
 import fetchAPI from './fetchAPI.js'
 
+
 const loadingContainer = document.querySelector(".loading-container")
 
+
 async function getUsers(num){
-  const {data} = await fetchAPI(`https://randomuser.me/api/?results=${num}`)
-  const users =  data.results
-  loadingContainer.classList.add("hide")
-  return users
+  const {data, error, loading} = await fetchAPI(`https://randomuser.me/api/?results=${num}`)
+
+
+  return [ data, error, loading]
 }
 
 
 async function displayUsers(){
-  const users = await getUsers(1000)
-
+  const [ data, error, loading] = await getUsers(100)
   const usersContainer = document.querySelector('.users-container')
-  createUserFromList(users, usersContainer)
-  eventListeners(users)
+
+  if(!loading){
+    loadingContainer.classList.add("hide")
+  }
+  if(error){
+    usersContainer.innerText = `${error} - Try refreshing the page`
+  }else{
+    const users = data.results
+    createListOfCountries(users)
+    createUserFromList(users, usersContainer)
+    eventListeners(users)
+  }
+}
+
+function createListOfCountries(users){
+  const listCountries = getCountries(users)
+  const selectCountry = document.querySelector(".select-country")
+
+  listCountries.forEach((country) => {
+    const option = document.createElement("option")
+    option.value = country
+    option.innerText = country
+
+    selectCountry.appendChild(option)
+
+  })
+}
+
+function getCountries(users){
+  const countries = users.map(user => {
+    return user.location.country
+  })
+
+  const onlyUnique = (value, index, self) => self.indexOf(value) === index;
+
+  return countries.filter(onlyUnique)
 }
 
 function createUserFromList(users, container){
@@ -38,18 +73,46 @@ function createUserFromList(users, container){
 }
 
 function eventListeners(users){
+  const usersContainer = document.querySelector(".users-container")
 
   const searchUsers = document.querySelector("#search-users");
   searchUsers.addEventListener("input", () => {
-    const usersContainer = document.querySelector(".users-container")
 
-    const filterUsers = users.filter((user) =>
+    const selectCountry = document.querySelector(".select-country")
+    let filterUsers;
+    if(selectCountry.value == "0") {
+      filterUsers = users.filter((user) =>
       `${user.name.first.toUpperCase()} ${user.name.last.toUpperCase()} `
-        .includes(searchUsers.value.toUpperCase()))
+        .includes(searchUsers.value.toUpperCase())
+      )
+
+    }else{
+      filterUsers = users.filter((user) =>
+      `${user.name.first.toUpperCase()} ${user.name.last.toUpperCase()} `
+        .includes(searchUsers.value.toUpperCase())
+          && user.location.country == selectCountry.value
+      )
+    }
 
     usersContainer.innerHTML = ''
-
     createUserFromList(filterUsers, usersContainer)
+  })
+
+  const selectCountry = document.querySelector(".select-country");
+
+  selectCountry.addEventListener("change", ({target}) => {
+    const searchUsers = document.querySelector("#search-users");
+    searchUsers.value = ''
+    usersContainer.innerHTML = ''
+
+    if(target.value != '0'){
+      const filterUsers = users.filter((user) => user.location.country == target.value)
+      createUserFromList(filterUsers, usersContainer)
+    }else{
+      createUserFromList(users, usersContainer)
+
+    }
+
   })
 
 }
